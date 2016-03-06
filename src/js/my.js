de7408a7f070e5518cc3ac7c333a2fb7625cd778
Yu <yu.gintoki@gmail.com>
2016-03-06 20:43:28 +0800

    v2.0.4
    头部

--- src/js/my.js
+++ src/js/my.js
@@ -1,3 +1,4 @@
+//
 function test(){
 	var test = document.getElementsByTagName('header')[0];
 	var cls = test.className;
@@ -14,11 +15,12 @@ elem 元素 ClassName 要修改的name NewClassName 要修改成的值
 2.与传入的classname比较，如果不符合，返回。如果符合，继续修改。
 3.修改classname，假如修改的值为空，那么修改为空
 4.返回classname.
-*/
+
 function classxg(elem,ClassName,NewClassName){
 	var cls = elem.className;
-	var new = new RegExp(ClassName);
+	var inew = new RegExp(ClassName);
 	if(new.test(cls)){
 		alert(1)
 	}
 }
+*/
